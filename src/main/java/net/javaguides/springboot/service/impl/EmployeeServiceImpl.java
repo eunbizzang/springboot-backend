d65a56bf3c657b597d523973150ad2c671c58ea6
eunbi <vysql123@naver.com>
2022-05-17 23:24:29 +0900

    0517

---
+++ src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package net.javaguides.springboot.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import net.javaguides.springboot.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+	private EmployeeRepository employeeRepository;
+	
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+}
